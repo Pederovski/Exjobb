f02735efe484b456a4a9aff21b3a2b51b3c54294
Pederovski <peder-d@live.se>
2022-03-24 14:57:07 +0100

    Hmmm
    Vad gör det här

---
+++ MyProject/Source/MyProject/MyTestClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//Det här är bara en test klass för att se om git fungerar
+#include "MyTestClass.h"
+
+MyTestClass::MyTestClass()
+{
+}
+
+MyTestClass::~MyTestClass()
+{
+}
---
+++ MyProject/Source/MyProject/MyTestClass.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+//Det här är bara en test klass för att se om git fungerar
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MYPROJECT_API MyTestClass
+{
+public:
+	MyTestClass();
+	~MyTestClass();
+};
